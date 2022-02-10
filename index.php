d427011d2785bf9d21cfe1cf28968383c740f8f5
Black-Loli <pundanya@gmail.com>
2022-02-10 22:21:39 +0300

    Измененён css и убран класс menu

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 </head>
 <body>
 <div class="black">
-    <a href="index.php" class="menu"> <img src="https://1.downloader.disk.yandex.ru/preview/f95757f920b6aa8fffa5293eed623ff129d3a312a9d7ab7e7a96de6d625c7f60/inf/ZeZoFfj2ti3eWULL7szGplH7THIOpr1QDSW2ndFJKTvRvHzgrI0RTB6S7N5ypsp0XbdOBn5hU9YTsWS2qKYmMw%3D%3D?uid=555148530&filename=Lovely%20Nails%20%283%29.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=555148530&tknv=v2&size=1838x937"></a>
+    <a href="index.php"> <img src="https://1.downloader.disk.yandex.ru/preview/f95757f920b6aa8fffa5293eed623ff129d3a312a9d7ab7e7a96de6d625c7f60/inf/ZeZoFfj2ti3eWULL7szGplH7THIOpr1QDSW2ndFJKTvRvHzgrI0RTB6S7N5ypsp0XbdOBn5hU9YTsWS2qKYmMw%3D%3D?uid=555148530&filename=Lovely%20Nails%20%283%29.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=555148530&tknv=v2&size=1838x937"></a>
     <a href="loading.php"> <img src="https://3.downloader.disk.yandex.ru/preview/fef01af9ac01de5e8b4a850679d8906ceb738fd086c0b035e765ada8c0a76028/inf/jA_kSFI5ol72RAKk83uu4kLC8ttpWQszL5u0tdolgdWI2PFmYyNPKz-mddwTLdfKEBC2vnsl9sfzhWkfHd3SHQ%3D%3D?uid=555148530&filename=head.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=555148530&tknv=v2&size=1838x937"></a>
 </div>
 

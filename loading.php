17c623a13dd580e01f1e072e0250cf91892ca026
Black-Loli <pundanya@gmail.com>
2022-02-13 22:05:03 +0300

    Начало работы с личным кабинетом

--- loading.php
+++ loading.php
@@ -5,10 +5,15 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style1.css" />
+    <link rel="stylesheet" href="style.css" />
     <title>Личный кабинет</title>
 </head>
 <body>
+<div class="black">
+    <a href="index.php"> <img src="https://1.downloader.disk.yandex.ru/preview/f95757f920b6aa8fffa5293eed623ff129d3a312a9d7ab7e7a96de6d625c7f60/inf/ZeZoFfj2ti3eWULL7szGplH7THIOpr1QDSW2ndFJKTvRvHzgrI0RTB6S7N5ypsp0XbdOBn5hU9YTsWS2qKYmMw%3D%3D?uid=555148530&filename=Lovely%20Nails%20%283%29.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=555148530&tknv=v2&size=1838x937"> </a>
+    <a href="loading.php"> <img src="https://3.downloader.disk.yandex.ru/preview/fef01af9ac01de5e8b4a850679d8906ceb738fd086c0b035e765ada8c0a76028/inf/jA_kSFI5ol72RAKk83uu4kLC8ttpWQszL5u0tdolgdWI2PFmYyNPKz-mddwTLdfKEBC2vnsl9sfzhWkfHd3SHQ%3D%3D?uid=555148530&filename=head.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=555148530&tknv=v2&size=1838x937"></a>
+</div>
+
     <p><img src="https://3.downloader.disk.yandex.ru/preview/8cdec1e3db940305f663b14ac22be102a756f7d565ae57939816885349ddf196/inf/KXdIQ_3fzcozpu5H4cEJbMyL83-yq2pgTiud_KxNDx1IZ39CeRa5QVJ0lX_oXTHrCIWmT4nxDyhPy3sGQO0Zmw%3D%3D?uid=555148530&filename=development.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=555148530&tknv=v2&size=1838x937"></p>
     <h1> Извините, личный кабинет разрабатывается, загляните позже</h1>
 </body>

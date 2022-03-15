1a9e9b58c0a935eb361833743b28a3f1d8e77f7a
Black-Loli <pundanya@gmail.com>
2022-03-15 18:27:25 +0300

    Вход в личный кабинет

--- loading.php
+++ loading.php
@@ -5,26 +5,18 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css" />
+
+    <link rel="stylesheet" href="style2.css" />
     <title>Личный кабинет</title>
 </head>
 <body>
-
     <div class="pass">
-        <div>
-            <a href="index.php"> <img src="https://1.downloader.disk.yandex.ru/preview/f95757f920b6aa8fffa5293eed623ff129d3a312a9d7ab7e7a96de6d625c7f60/inf/ZeZoFfj2ti3eWULL7szGplH7THIOpr1QDSW2ndFJKTvRvHzgrI0RTB6S7N5ypsp0XbdOBn5hU9YTsWS2qKYmMw%3D%3D?uid=555148530&filename=Lovely%20Nails%20%283%29.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=555148530&tknv=v2&size=1838x937" alt="Азбука Программирования"> </a>
-
-        <div>
-            <div>
-                <label for="login"> Ваш логин </label>
-            </div>
-            <span>
-                <input type="text" id="login" placeholder="Логин или почта">
-            </span>
-            <div class="line"></div>
-        </div>
-        </div>
-
+        <form action="#" class="pass1">
+            <img src="https://1.downloader.disk.yandex.ru/preview/f95757f920b6aa8fffa5293eed623ff129d3a312a9d7ab7e7a96de6d625c7f60/inf/ZeZoFfj2ti3eWULL7szGplH7THIOpr1QDSW2ndFJKTvRvHzgrI0RTB6S7N5ypsp0XbdOBn5hU9YTsWS2qKYmMw%3D%3D?uid=555148530&filename=Lovely%20Nails%20%283%29.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=555148530&tknv=v2&size=1898x927">
+            <input type="email" placeholder="Введите свой email" required="" />
+            <input type="password" placeholder="Введите пароль" required="" minlength="6" maxlength="12" />
+            <button type="submit" class="btn btn-outline-dark"> Войти </button>
+        </form>
     </div>
 </body>
 </html>

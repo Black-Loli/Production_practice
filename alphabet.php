5169cc87b91560be574ec39bcb0dbb5750b9bc7e
Black-Loli <pundanya@gmail.com>
2022-03-31 13:23:35 +0300

    Изменение логотипа с картинки на текст

--- alphabet.php
+++ alphabet.php
@@ -11,7 +11,7 @@
 </head>
 <body>
 <div class="black">
-    <a href="modul1.php"> <img src="https://1.downloader.disk.yandex.ru/preview/cdeb87ec47c65cd3f9239edd7ebdb5487556db024b2ed5939c7f28ffd5e75ebf/inf/xcEEmTPdsT8Xo4E7kJv-OUU_bOVXEM1hBaP5VE6DWGyI00c38YuZUDrwfLJChR5H8iDL9hZHlgUuKC5m4YvyZQ%3D%3D?uid=555148530&filename=Lovely_Nails__3_-removebg-preview.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=555148530&tknv=v2&size=1898x927"></a>
+    <a href="index.php" class="ap"> Азбука <br> Программирования </a>
     <a href="entrance.php"> <img src="https://3.downloader.disk.yandex.ru/preview/fef01af9ac01de5e8b4a850679d8906ceb738fd086c0b035e765ada8c0a76028/inf/jA_kSFI5ol72RAKk83uu4kLC8ttpWQszL5u0tdolgdWI2PFmYyNPKz-mddwTLdfKEBC2vnsl9sfzhWkfHd3SHQ%3D%3D?uid=555148530&filename=head.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=555148530&tknv=v2&size=1838x937"></a>
 </div>
 
